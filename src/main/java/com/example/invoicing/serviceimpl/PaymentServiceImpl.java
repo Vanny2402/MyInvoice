21085ce40cc19c48940d08c7d59c27a336f1e026
Vanny2402 <79197656+Vanny2402@users.noreply.github.com>
2026-03-17 19:31:07 +0700

    newDb

--- src/main/java/com/example/invoicing/serviceimpl/PaymentServiceImpl.java
+++ src/main/java/com/example/invoicing/serviceimpl/PaymentServiceImpl.java
@@ -3,7 +3,6 @@ package com.example.invoicing.serviceimpl;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.invoicing.dto.PaymentDTO;
